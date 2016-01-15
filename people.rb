c80055ddb78a672d851d94ae4e8f8657b00bf1be
Lyubomr <s.lyubomyr@gmail.com>
2016-01-15 16:38:10 -0500

    Added Chris and Cristina)

--- people.rb
+++ people.rb
@@ -1,15 +1,20 @@
-
+# Created person class
 class Person
   attr_accessor :name
 
   def initialize(name)
-    @name
+    @name = name
   end
+# greeting method
+def greeting
+  puts "Hi, my name is #{name}!"
+end
+
 end # Ends person class
 
 
 #Created Student class
-class Student
+class Student < Person
 
   def learn
     puts "I get it!"
@@ -19,9 +24,21 @@ end #ends Student class
 
 
 # Created Instructor class
-class Instructor
+class Instructor < Person
 
   def teach
     puts "Everything in Ruby is an Object"
   end
+
 end # ends Instructor class
+
+chris = Instructor.new("Chris")
+chris.greeting
+
+cristina = Student.new("Cristina")
+cristina.greeting
+
+chris.teach
+cristina.learn
+
+chris.learn
